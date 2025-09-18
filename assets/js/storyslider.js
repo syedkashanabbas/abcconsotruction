d3171d35b7a35a5d2801fd93ae561326186b6eca
SyedKashanAbbasNaqvi <abbaskashan234@gmail.com>
2025-09-18 21:49:48 +0500

    chanegs

--- assets/js/storyslider.js
+++ assets/js/storyslider.js
@@ -4,16 +4,17 @@ let index = 2; // start at center
 
 function updateCards() {
   cards.forEach((card, i) => {
-    card.classList.remove("scale-100", "opacity-100", "z-20", "scale-90", "opacity-50", "z-10");
+    card.classList.remove("scale-125", "scale-90", "opacity-100", "opacity-50", "z-20", "z-10");
 
     if (i === index) {
       card.classList.add("scale-125", "opacity-100", "z-20"); // center big
     } else {
-      card.classList.add("scale-90", "opacity-50", "z-10"); // sides small
+      card.classList.add("scale-90", "opacity-50", "z-10"); // side smaller
     }
   });
 
-  slider.style.transform = `translateX(calc(50% - ${index * 22}rem))`;
+  // spacing calc updated for wider cards
+  slider.style.transform = `translateX(calc(50% - ${index * 26}rem))`;
 }
 
 document.getElementById("next").addEventListener("click", () => {
